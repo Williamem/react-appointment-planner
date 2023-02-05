39ac13008f6a0f5b10fe39ff3cd9524dc34d3200
WilliamEm <william.emanuelsson@gmail.com>
2023-02-05 08:55:02 +0800

    add render logic

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-export const Tile = () => {
+
+export const Tile = ({tile}) => {
+  const obj = Object.values(tile);
   return (
     <div className="tile-container">
-      
+      {
+        obj.map((item, index) => {
+          return <p key={index} className={index === 0 ? 'tile-title' : 'tile'}>
+            {item}
+          </p>
+        })
+      }
     </div>
   );
 };
--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Tile } from '../tile/Tile';
 
-export const TileList = () => {
+export const TileList = ({dataList}) => {
   return (
     <div>
-      
+      {
+        dataList.map((tile, index) => {
+          return (<Tile 
+            key={index}
+            tile={tile}
+          />)
+        })
+      }
     </div>
   );
 };
