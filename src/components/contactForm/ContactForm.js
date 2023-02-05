6d6e7109936a3b7ced2dc166e8206b29ca30212f
WilliamEm <william.emanuelsson@gmail.com>
2023-02-05 08:54:49 +0800

    add logic

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,15 +1,39 @@
 import React from "react";
 
-export const ContactForm = ({
-  name,
-  setName,
-  phone,
-  setPhone,
-  email,
-  setEmail,
-  handleSubmit
-}) => {
+export const ContactForm = (props) => {
   return (
-    ContactForm
+    <form onSubmit={props.handleSubmit}>
+      <input
+        type='text'
+        placeholder="Name"
+        name="name"
+        onChange={props.handleChange}
+        value={props.values.name}>
+      </input>
+      <input
+        type='tel'
+        placeholder="Phone Number"
+        name="phone"
+        onChange={props.handleChange}
+        value={props.values.phone}>
+      </input>
+      <input
+        type='email'
+        placeholder="E-Mail"
+        name="email"
+        onChange={props.handleChange}
+        value={props.values.email}>
+      </input>
+      <button>Submit</button>
+    </form>
   );
 };
+
+
+/* name,
+setName,
+phone,
+setPhone,
+email,
+setEmail,
+handleSubmit */
